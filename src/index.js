3d7cae8e1da2c463d22a125025c1f34c999356e5
koretskiyav <koretskiyav@gmail.com>
2021-05-25 20:20:47 +0300

    add class component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom';
 import './index.css';
 
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById('root'));
+import App from './components/app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
