fd7f7f5a6d40c1da53b30e68ab176f02efd116cc
koretskiyav <koretskiyav@gmail.com>
2021-05-25 21:13:20 +0300

    add custom hook useAmount

--- src/components/product.js
+++ src/components/product.js
@@ -1,8 +1,15 @@
+import useAmount from '../hooks/use-amount';
+
 export default function Product(props) {
+  const { amount, decrement, increment } = useAmount(0);
+
   return (
     <div>
       <p>{props.product.name}</p>
       <p>{props.product.price} $</p>
+      <button onClick={decrement}>-</button>
+      {amount}
+      <button onClick={increment}>+</button>
     </div>
   );
 }
