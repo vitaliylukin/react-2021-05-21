68cf8dfbcb413849b61e60691215f46f1c8fee3d
koretskiyav <koretskiyav@gmail.com>
2021-05-28 20:25:08 +0300

    add prop-types

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styles from './product.module.css';
 import { ReactComponent as Minus } from '../../icons/minus.svg';
 import { ReactComponent as Plus } from '../../icons/plus.svg';
@@ -28,5 +29,16 @@ const Product = ({ product, amount, increment, decrement }) => (
     </div>
   </div>
 );
+
+Product.propTypes = {
+  product: PropTypes.shape({
+    name: PropTypes.string,
+    price: PropTypes.number,
+    ingredients: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  }).isRequired,
+  amount: PropTypes.number,
+  increment: PropTypes.func,
+  decrement: PropTypes.func,
+};
 
 export default counter(Product);
