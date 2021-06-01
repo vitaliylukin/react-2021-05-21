c55b803a55e46bb0723e01d99b5a60071c6a80cb
koretskiyav <koretskiyav@gmail.com>
2021-06-01 12:55:58 +0300

    HW 2.2 add Product tests

--- src/components/product/product.test.js
+++ src/components/product/product.test.js
@@ -1,33 +1,59 @@
-import Enzyme, { mount } from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { mount } from 'enzyme';
 import Product from './product';
 
 import { restaurants } from '../../fixtures';
 
-Enzyme.configure({ adapter: new Adapter() });
-
 const product = restaurants[0].menu[0];
+
+function render(props) {
+  const wrapper = mount(<Product {...props} />);
+  const getByDataId = (dataId) => wrapper.find(`[data-id="${dataId}"]`);
+
+  return {
+    getProductsCount: () => getByDataId('product').length,
+    getAmount: () => getByDataId('product-amount').text(),
+    increase: () => getByDataId('product-increment').simulate('click'),
+    decrease: () => getByDataId('product-decrement').simulate('click'),
+  };
+}
 
 describe('Product', () => {
   it('should render', () => {
-    const wrapper = mount(<Product product={product} />);
-    expect(wrapper.find('[data-id="product"]').length).toBe(1);
+    const testKit = render({ product });
+    expect(testKit.getProductsCount()).toBe(1);
   });
 
   it('should init from 0 amount', () => {
-    const wrapper = mount(<Product product={product} />);
-    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('0');
+    const testKit = render({ product });
+    expect(testKit.getAmount()).toBe('0');
   });
 
   it('should increment amount', () => {
-    const wrapper = mount(<Product product={product} />);
-    wrapper.find('[data-id="product-increment"]').simulate('click');
-    expect(wrapper.find('[data-id="product-amount"]').text()).toBe('1');
+    const testKit = render({ product });
+    testKit.increase();
+    expect(testKit.getAmount()).toBe('1');
   });
 
   it('should fetch data on mount', () => {
     const fn = jest.fn();
-    mount(<Product product={product} fetchData={fn} />);
+    render({ product, fetchData: fn });
     expect(fn).toBeCalledWith(product.id);
+  });
+
+  it('should init with amount 2', () => {
+    const testKit = render({ product, initialCount: 2 });
+    expect(testKit.getAmount()).toBe('2');
+  });
+
+  it('should decrement amount', () => {
+    const testKit = render({ product, initialCount: 4 });
+    testKit.decrease();
+    expect(testKit.getAmount()).toBe('3');
+  });
+
+  it("shouldn't decrement with 0 amount", () => {
+    const testKit = render({ product });
+    testKit.decrease();
+    expect(testKit.getAmount()).toBe('0');
   });
 });
