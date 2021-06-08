e4124f6a37907d1cf8da26aecc95adb774194fff
koretskiyav <koretskiyav@gmail.com>
2021-06-08 20:28:43 +0300

    add api middleware

--- src/components/restaurants/restaurants.js
+++ src/components/restaurants/restaurants.js
@@ -1,19 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Restaurant from '../restaurant';
 import Tabs from '../tabs';
+import Loader from '../loader';
 import { restaurantsListSelector } from '../../redux/selectors';
+import { loadRestaurants } from '../../redux/actions';
 
-const Restaurants = ({ restaurants }) => {
-  const [activeId, setActiveId] = useState(restaurants[0].id);
+const Restaurants = ({ restaurants, loadRestaurants }) => {
+  const [activeId, setActiveId] = useState(restaurants[0]?.id);
+
+  useEffect(() => {
+    loadRestaurants();
+  }, []); // eslint-disable-line
+
+  const restaurantId = activeId || restaurants[0]?.id;
+
+  if (restaurants.length === 0) return <Loader />;
 
   const tabs = restaurants.map(({ id, name }) => ({ id, title: name }));
 
   return (
     <div>
-      <Tabs tabs={tabs} activeId={activeId} onChange={setActiveId} />
-      <Restaurant id={activeId} />
+      <Tabs tabs={tabs} activeId={restaurantId} onChange={setActiveId} />
+      <Restaurant id={restaurantId} />
     </div>
   );
 };
@@ -30,4 +40,4 @@ const mapStateToProps = (state) => ({
   restaurants: restaurantsListSelector(state),
 });
 
-export default connect(mapStateToProps)(Restaurants);
+export default connect(mapStateToProps, { loadRestaurants })(Restaurants);
