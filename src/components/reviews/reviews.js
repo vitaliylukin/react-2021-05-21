1557e9512dba2a3bc297c50907417eac4261ea7f
koretskiyav <koretskiyav@gmail.com>
2021-06-08 21:04:39 +0300

    add redux-thunk

--- src/components/reviews/reviews.js
+++ src/components/reviews/reviews.js
@@ -1,9 +1,17 @@
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Review from './review';
 import ReviewForm from './review-form';
 import styles from './reviews.module.css';
 
-const Reviews = ({ reviews, restaurantId }) => {
+import { loadReviews } from '../../redux/actions';
+import { useEffect } from 'react';
+
+const Reviews = ({ reviews, restaurantId, loadReviews }) => {
+  useEffect(() => {
+    loadReviews(restaurantId);
+  }, [loadReviews, restaurantId]);
+
   return (
     <div className={styles.reviews}>
       {reviews.map((id) => (
@@ -19,4 +27,4 @@ Reviews.propTypes = {
   reviews: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 };
 
-export default Reviews;
+export default connect(null, { loadReviews })(Reviews);
